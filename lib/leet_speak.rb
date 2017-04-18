89e324bed717ca927858981c1200fca7fe1e558f
jin camou and Steven Galvin <jin@>
2017-04-18 15:25:56 -0700

    test out code to convert the letter s into the letter z.

--- lib/leet_speak.rb
+++ lib/leet_speak.rb
@@ -7,6 +7,10 @@ class String
         leet_array.push("3")
       elsif letter.==("o")
         leet_array.push("0")
+      elsif letter.==("I")
+        leet_array.push("1")
+      elsif letter.==("s")
+        leet_array.push("z")
       else
         leet_array.push(letter)
       end
